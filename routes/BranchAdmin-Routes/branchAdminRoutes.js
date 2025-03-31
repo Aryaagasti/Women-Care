1770ed224f67a0fc5ccf268fc7e0895ccff8c061
Arya agasti <aryaagasti@gmail.com>
2025-03-31 12:55:39 +0530

    adding update routes superadmin and branch adding middleware

--- routes/BranchAdmin-Routes/branchAdminRoutes.js
+++ routes/BranchAdmin-Routes/branchAdminRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {register,login,forgotPassword,verifyOtp,resetPassword,logout, getBranchAdminProfile, updateBranchAdminProfile, changeAdminPassAtProfile} =  require('../../controllers/BranchAdmin-Controllers/branchAdminController')
 const router = express.Router();
+const branchAdminAuthMiddleware =  require('../../middlewares/branchMiddleware')
 const { upload } = require('../../config/cloudinary');
  
 router.post('/register', upload.fields([
@@ -12,10 +13,10 @@ router.put('/updateProfile/:id', upload.fields([
     { name: "profileImage", maxCount: 1 },
   ]), updateBranchAdminProfile);
 router.post('/reset-password',resetPassword);
-router.get('/logout',logout);
-router.get('/getProfile/:id', getBranchAdminProfile);
+router.get('/logout',branchAdminAuthMiddleware,logout);
+router.get('/getProfile/:id', branchAdminAuthMiddleware ,getBranchAdminProfile);
 
 
-router.put('/change-password/:id', changeAdminPassAtProfile);
+router.put('/change-password/:id',branchAdminAuthMiddleware ,changeAdminPassAtProfile);
  
 module.exports = router;
--- routes/SuperAdmin-Routes/Super-AdminRoutes.js
+++ routes/SuperAdmin-Routes/Super-AdminRoutes.js
@@ -1,22 +1,30 @@
 const express = require('express');
-const {register, login, resetPassword, forgotPassword, verifyOtp, logout, updateAdminProfile, getAdminProfile, changeAdminPasswordAtProfile} =  require('../../controllers/SuperAdmin-Controllers/SuperAdminController')
+const {
+    register,
+    login,
+    resetPassword,
+    forgotPassword,
+    verifyOtp,
+    logout,
+    updateAdminProfile,
+    getAdminProfile,
+    changeAdminPasswordAtProfile
+} = require('../../controllers/SuperAdmin-Controllers/SuperAdminController');
+
 const { upload } = require('../../config/cloudinary');
- 
+const superAdminAuthMiddleware = require('../../middlewares/superAdminMiddleware');
+
 const router = express.Router();
- 
+
 // Admin Routes
-router.post('/register',upload.fields([
-    { name: "profileImage", maxCount: 1 },
-  ]),register)
+router.post('/register', upload.fields([{ name: "profileImage", maxCount: 1 }]), register);
 router.post('/login', login);
 router.post('/forgot-password', forgotPassword);
 router.post('/verify-otp', verifyOtp);
 router.post('/reset-password', resetPassword);
-router.put('/UpdateProfile/:id', upload.fields([
-  { name: "profileImage", maxCount: 1 },
-]), updateAdminProfile);
-router.post('/logout', logout);
-router.get('/get-admin-profile/:id', getAdminProfile);
-router.put('/change-password/:id', changeAdminPasswordAtProfile);
- 
+router.put('/UpdateProfile/:id', superAdminAuthMiddleware, upload.fields([{ name: "profileImage", maxCount: 1 }]), updateAdminProfile);
+router.post('/logout', superAdminAuthMiddleware ,logout);
+router.get('/get-admin-profile/:id', superAdminAuthMiddleware, getAdminProfile);
+router.put('/change-password/:id', superAdminAuthMiddleware, changeAdminPasswordAtProfile);
+
 module.exports = router;
